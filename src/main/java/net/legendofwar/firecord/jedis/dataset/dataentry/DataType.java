f29262aca8999ef2e0dc714046da64c79f436d02
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2023-03-10 13:42:56 +0100

    add more unimplemented DataType entries

--- src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/DataType.java
+++ src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/DataType.java
@@ -5,27 +5,44 @@ import net.legendofwar.firecord.jedis.dataset.dataentry.simple.RInteger;
 import net.legendofwar.firecord.jedis.dataset.dataentry.simple.RItemStack;
 
 public enum DataType {
-    
 
+    // small
     BOOLEAN(null),
+    // numeric
     BYTE(null),
-    CHAR(null),
     SHORT(null),
     INTEGER(RInteger.class),
     LONG(null),
     FLOAT(null),
     DOUBLE(null),
+
+    CHAR(null),
     STRING(null),
     UUID(null),
+    // mc
+    LOCATION(null),
+    VECTOR(null),
+    
+    //large
     ITEMSTACK(RItemStack.class),
     INVENTORY(null),
-    LOCATION(null),
+    BYTEARRAY(null), // generalized QoL
 
+    // composite types
+
+    // corresponds to a native redis type
     LIST(RList.class),
     SET(null),
     SORTEDSET(null),
     MAP(null),
-    OBJECT(null); //for later use
+
+    // java types we emulate on redis
+    QUEUE(null),
+    STACK(null),
+
+    // for later use in custom objects
+    WRAPPER(null),
+    OBJECT(null); 
 
     Class<?> c;
 
