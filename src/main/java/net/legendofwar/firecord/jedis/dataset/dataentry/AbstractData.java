049f366958a601127939343fffa183a2b04a25c6
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2023-03-12 15:34:49 +0100

    add support for creating sync-objects

--- src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/AbstractData.java
+++ src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/AbstractData.java
@@ -2,20 +2,50 @@ package net.legendofwar.firecord.jedis.dataset.dataentry;
 
 import java.io.Closeable;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 
 import org.jetbrains.annotations.NotNull;
 
 import net.legendofwar.firecord.jedis.ClassicJedisPool;
 import net.legendofwar.firecord.jedis.JedisLock;
+import net.legendofwar.firecord.jedis.dataset.dataentry.object.AbstractObject;
+import net.legendofwar.firecord.jedis.dataset.dataentry.simple.Invalid;
 import redis.clients.jedis.Jedis;
 
 public abstract class AbstractData<T> implements Closeable {
 
-    private static AbstractData<?> callConstructor(@NotNull String key, @NotNull Class<?> c) {
+    public static AbstractData<?> callConstructor(@NotNull String key, @NotNull Class<?> c) {
 
         try {
             return (AbstractData<?>) c.getDeclaredConstructor(String.class).newInstance(key);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * This calls the constructor that provides a default value
+     * @param key
+     * @param c
+     * @param defaultValue
+     * @return
+     */
+    public static AbstractData<?> callConstructor(@NotNull String key, @NotNull Class<?> c, Object defaultValue) {
+
+        try {
+            return (AbstractData<?>) c.getDeclaredConstructor(String.class, defaultValue.getClass()).newInstance(key, defaultValue);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (SecurityException e) {
@@ -43,6 +73,12 @@ public abstract class AbstractData<T> implements Closeable {
      * @return
      */
     public static AbstractData<?> create(@NotNull String key) {
+
+        // since reflection is used the @NotNull annotation does not guarantee null safety
+        if (key == null){
+            return null;
+        }
+
         // we don't need to recreate
         synchronized (loaded) {
             if (loaded.containsKey(key)) {
@@ -61,7 +97,30 @@ public abstract class AbstractData<T> implements Closeable {
                 e.printStackTrace();
             }
             if (dt != null) {
-                return callConstructor(key, dt.getC());
+                if (dt != DataType.OBJECT) {
+                    if (dt.canBeLoaded()) {
+                        return callConstructor(key, dt.getC());
+                    } else {
+                        return new Invalid(key);
+                    }
+                } else {
+                    String className;
+                    try (Jedis j = ClassicJedisPool.getJedis()) {
+                        className = j.get(key + ":class");
+                    }
+                    Class<?> c = null;
+                    if (className != null) {
+                        try {
+                            c = Class.forName(className);
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (c != null && AbstractObject.class.isAssignableFrom(c)
+                            && !Modifier.isAbstract(c.getModifiers())) {
+                        return callConstructor(key, c);
+                    }
+                }
             }
         }
         return null;
