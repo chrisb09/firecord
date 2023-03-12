a935d27303ed8488af2f7b9845277b9aad0dbb7b
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2023-03-12 15:36:22 +0100

    add invalid data type

---
+++ src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/simple/Invalid.java
@@ -0,0 +1,12 @@
+package net.legendofwar.firecord.jedis.dataset.dataentry.simple;
+
+import net.legendofwar.firecord.jedis.dataset.dataentry.AbstractData;
+
+public class Invalid extends AbstractData<String> {
+
+    public Invalid(String key) {
+        super(key);
+    }
+
+
+}
