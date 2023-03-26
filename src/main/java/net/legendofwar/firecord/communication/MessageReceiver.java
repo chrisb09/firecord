b0bac610eb06b453c5b2f0273f9b33efd03cf177
FreeSoccerHDX <freesoccerhdx@gmail.com>
2023-03-26 12:22:35 +0200

    Add Docs to MessageReceiver

--- src/main/java/net/legendofwar/firecord/communication/MessageReceiver.java
+++ src/main/java/net/legendofwar/firecord/communication/MessageReceiver.java
@@ -1,7 +1,15 @@
 package net.legendofwar.firecord.communication;
 
 public interface MessageReceiver {
-    
-    public void receive(String channel, String sender, boolean broadcast, String message);
+
+    /**
+     * Simple method to receive a message from the redis server
+     *
+     * @param channel The channel the message was sent to
+     * @param sender The sender of the message
+     * @param broadcast Whether the message was sent to all servers or not
+     * @param message The message itself
+     * */
+    void receive(String channel, String sender, boolean broadcast, String message);
 
 }
