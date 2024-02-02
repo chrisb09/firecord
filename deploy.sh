20da46b4af1c3f97a3ffe1dddf271d60ee9e7003
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-02-02 10:52:48 +0100

    use rsync for disable

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,15 @@ if [ "$current_user" = "christian" ]; then
 
 echo "Running for user christian..."
 
-
-if [[ -z "$(ls -A /mnt/legendofwar)" ]]; then
-    ~/SSH/tool/linux_ssh.sh "echo 'Unlocked keepass...'"
-    echo "Mount is empty. Mounting..."
-    sshfs root@legendofwar.net:/ /mnt/legendofwar -p 22
-else
-    echo "/mnt/legendofwar already mounted."
-fi
+~/SSH/tool/linux_ssh.sh "echo 'Unlocked keepass...'"
 
 echo "Maven test, compile, package & install"
 mvn clean install
 echo "Copy files to remote staging folder"
-find target/ -type f -regex '.*/[A-Za-z]+-[0-9]+\.[0-9]+\.jar' \
+find target/ -type f -regex '.*/[A-Za-z]+-[0-9]+\.[0-9]+\(\.[0-9]+\)?\(\.[0-9]+\)?\.jar' \
  -exec echo {} \; \
- -exec cp {} /mnt/legendofwar/minecraft/staging/ \;
-echo "Copy from staging to paths"
+ -exec rsync {} legendofwar@legendofwar.net:/minecraft/staging/ --progress \;
+echo "Copied to staging, now distributing it on the server..."
 ssh legendofwar@legendofwar.net 'find /minecraft/staging/ -type f \
  -exec echo {} \; \
  -exec cp {} /minecraft/server/paper/lobby/custom_plugins/ \; \
@@ -30,6 +23,7 @@ ssh legendofwar@legendofwar.net 'find /minecraft/staging/ -type f \
  -exec cp {} /minecraft/server/multipaper/alpha/plugins/ \; \
  -exec cp {} /minecraft/server/multipaper/bravo/plugins/ \; \
  -delete'
+
 echo "Done"
 
 elif [ "$current_user" = "legendofwar" ]; then
