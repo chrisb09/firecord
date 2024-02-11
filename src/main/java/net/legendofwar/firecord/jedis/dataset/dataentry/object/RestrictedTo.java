64a380240f010920508915c30f1dac4b252de98d
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-02-11 12:35:14 +0100

    fix mistake: confused "Method" with "Type" ...

--- src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/object/RestrictedTo.java
+++ src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/object/RestrictedTo.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 import net.legendofwar.firecord.tool.NodeType;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD})
+@Target({ElementType.FIELD, ElementType.TYPE})
 public @interface RestrictedTo {
     NodeType type() default NodeType.ANY;
 }
