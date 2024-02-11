91872fa79861154512f9b8e91743144131c69b71
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-02-11 12:10:18 +0100

    add staticinit annotation

---
+++ src/main/java/net/legendofwar/firecord/jedis/dataset/dataentry/object/StaticInit.java
@@ -0,0 +1,11 @@
+package net.legendofwar.firecord.jedis.dataset.dataentry.object;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface StaticInit {
+}
