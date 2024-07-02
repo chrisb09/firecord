2fd319a153b683c1a7d01ec4c32720ac8c67f898
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-07-02 18:04:42 +0200

    add set Commiction ID enums

--- src/main/java/net/legendofwar/firecord/communication/JedisCommunicationChannel.java
+++ src/main/java/net/legendofwar/firecord/communication/JedisCommunicationChannel.java
@@ -32,6 +32,11 @@ public enum JedisCommunicationChannel implements ByteDataInterface {
     MAP_PUT(23),
     MAP_PUT_ALL(24),
     MAP_REMOVE(25),
+    SET_ADD(26),
+    SET_ADD_ALL(27),
+    SET_REMOVE(28),
+    SET_REMOVE_ALL(29),
+    SET_RETAIN_ALL(30),
     REFERENCE_UPDATE(50),               // Informs about a changed reference in an AbstractObject
     OBJECT_OVERWRITE(51),               // Used when a temp entry creates an object
     CHAT_BROADCAST(100),
