7750fb4ebf4a9186df75ad3552901e64f68bd8b5
FreeSoccerHDX <unconfigured@null.spigotmc.org>
2024-12-19 00:38:50 +0100

    Fix doc SimpleData

---
+++ src/main/java/net/legendofwar/firecord/tool/GlobalParams.java
@@ -0,0 +1,9 @@
+package net.legendofwar.firecord.tool;
+
+public class GlobalParams {
+
+    // Wenn true: Spieler können nur andere Völker angreifen.
+    // Wenn false: Spieler können auch das eigene Volk angreifen.
+    public static boolean PVP_SEPERATED_BY_VOLK = false;
+
+}
