e4c8a09280976c2197bc938473af295335b935df
Christian F. Brinkmann <christian.brinkmann09@gmail.com>
2024-12-22 15:11:27 +0100

    move GlobalParams to cinder

--- src/main/java/net/legendofwar/firecord/tool/GlobalParams.java
+++
@@ -1,9 +0,0 @@
-package net.legendofwar.firecord.tool;
-
-public class GlobalParams {
-
-    // Wenn true: Spieler können nur andere Völker angreifen.
-    // Wenn false: Spieler können auch das eigene Volk angreifen.
-    public static boolean PVP_SEPERATED_BY_VOLK = false;
-
-}
